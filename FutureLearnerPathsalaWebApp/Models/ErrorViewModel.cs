f4bca67c2f2fd10ca534f1f9732297cd6e84e8d9
Sumit P <sumitpramanik19818@outlook.com>
2021-10-18 20:49:06 +0530

    first commit

---
+++ FutureLearnerPathsalaWebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace FutureLearnerPathsalaWebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
